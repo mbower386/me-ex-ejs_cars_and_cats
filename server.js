7479db44a1d99b2ba5f2f0b77dc72290cf4b8345
mbower386 <mbower386@gmail.com>
2019-07-16 17:00:38 -0700

    adding new views

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname + "/static"));
+
+app.set('views', __dirname + "/views");
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.send(index.html);
+});
+
+app.get('/cats', function (request, response) {
+  response.render('cats');
+});
+
+app.get('/cars', function (request, response) {
+  response.render('cars');
+});
+
+app.get('/cars/new', function (request, response) {
+  response.render('form');
+});
+
+app.listen(6789, function () {
+  console.log("listening on port 6789");
+});
